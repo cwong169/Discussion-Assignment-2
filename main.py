a49e905d3feda6ed8495c094f4d2a1207d6d0276
cwong169 <118635527+cwong169@users.noreply.github.com>
2022-11-20 22:52:53 -0800

    Update main.py

--- main.py
+++ main.py
@@ -13,11 +13,46 @@ def multi_sort(arr, cmp, method="None"):
     else:
         print("invalid argument!")
 
-
-
+def cmp(a,b):
+    if a < b:
+        return -1
+    elif a == b:
+        return 0
+    else:
+        return 1
 
 # must be in-place sort
 def merge_sort(arr,cmp):
+    if len(arr) > 1:
+    # Find the floor
+    mid = len(arr) // 2
+    # Left represents first half
+    left = arr[:mid]
+    # Right represents second half
+    right = arr[mid:]
+    # Sort first half
+    merge_sort(left,cmp)
+    # Sort second half
+    merge_sort(right,cmp)
+    i = j = k = 0
+    while i < len(left) and j < len(right):
+        if cmp(left[i],right[j]) <= 0:
+            arr[k] = left[i]
+            i += 1
+        else:
+            arr[k] = right[j]
+            j += 1
+        k += 1
+    # Check for remaining elements
+    while i < len(left):
+        arr[k] = left[i]
+        i += 1
+        k += 1
+    while j < len(right):
+        arr[k] = right[j]
+        j += 1
+        k += 1
+    return arr
     pass
 
 # must be in-place sort
